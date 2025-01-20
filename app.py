7af567cd398273d0f1a146f55c5603b7e5b95a9f
Alpana-Omnihire <alpana@omnihire.app>
2025-01-20 14:12:43 +0530

    Added login functionality to authenticate users and return JWT tokens.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource, Api
+from flask_jwt_extended import create_access_token, JWTManager
 
 app = Flask(__name__, template_folder="templates")
 
@@ -9,13 +10,48 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 db = SQLAlchemy(app)
 api = Api(app)
+jwt = JWTManager(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
-db.create_all()
+with app.app_context():
+    db.create_all()
+
+class UserRegistration(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+
+        if not username or not password:
+            return {'message': 'Missing username or password'}, 400
+        if User.query.filter_by(username=username).first():
+            return {'message': 'Username already taken'}, 400
+        
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return {'message':'User created successfully'}, 200
+    
+class UserLogin(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            access_token = create_access_token(identity=user.id)
+            return {'access_token': access_token}
+        
+        return {'message': 'Invalid credentials'}, 401
+    
+api.add_resource(UserRegistration, '/register')
+api.add_resource(UserLogin, '/login')
 
 tasks = []
 
